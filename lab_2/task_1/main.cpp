da7ebab27684e340a4c90973c63ec7ceb0962d7b
Alvi Hasan <livealvi@gmail.com>
2023-06-14 21:52:04 +0600

    update: lab_2 > task_1

--- lab_2/task_1/main.cpp
+++ lab_2/task_1/main.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
 using namespace std;
+int number;
 
-void checkToken(char token[100]){
-    cout<<"==================="<<endl;
-    for (int i = 0; i < 100; i++) {
+void checkToken(char token[number]){
+    int arithmeticCount=0, alphabetCount=0, punctuationCount=0, numberCount=0;
+    for (int i = 0; i < number; i++) {
         if (token[i] == '+' || token[i] == '-' || token[i] == '*' || token[i] == '/' || token[i] == '%') {
-            cout << "the arithmetic operators: " << token[i] << "\n";
+            arithmeticCount++;
+            cout << arithmeticCount << ")"<< " arithmetic: " << token[i] << "\n";
         }
     }
-    cout<<"==================="<<endl;
-    for (int i = 0; i < 100; i++) {
+    if (arithmeticCount >=1)  cout << "-------------------\ntotal arithmetics: " << arithmeticCount << "\n"
+    << endl;
+    for (int i = 0; i < number; i++) {
+        if (token[i] == '0' || token[i] == '1' || token[i] == '2' || token[i] == '3' || token[i] == '4' || token[i] == '5' ||
+        token[i] == '6' || token[i] == '7' || token[i] == '8' || token[i] == '9') {
+            numberCount++;
+            cout << numberCount << ")"<< " number: " << token[i] << "\n";
+        }
+    }
+    if (numberCount >=1)  cout << "-------------------\ntotal numbers: " << numberCount << "\n" << endl;
+    for (int i = 0; i <= number; i++) {
         if(token[i] == 'a' || token[i] == 'b' || token[i] == 'c' || token[i] == 'd' || token[i] == 'e' || token[i] == 'f' ||
            token[i] == 'g' || token[i] == 'h' || token[i] == 'i' || token[i] == 'j'  || token[i] == 'l' || token[i] == 'm' || token[i]
-                                                                                                                  == 'n' || token[i] == 'o' || token[i] == 'p' || token[i] == 'q' || token[i] == 'r' || token[i] == 's' || token[i] == 't'
+           == 'n' || token[i] == 'o' || token[i] == 'p' || token[i] == 'q' || token[i] == 'r' || token[i] == 's' || token[i] == 't'
            || token[i] == 'u' || token[i] == 'v' || token[i] == 'x' || token[i] == 'y' || token[i] == 'z') {
-            cout << "the alphabet: " << token[i] << "\n";
+            alphabetCount++;
+            cout << alphabetCount << ")"<< " alphabet: " << token[i] << "\n";
         }
     }
-    cout<<"==================="<<endl;
-    for (int i = 0; i < 100; i++) {
+    if (alphabetCount >=1)  cout << "-------------------\ntotal alphabets: " << alphabetCount << "\n" << endl;
+    for (int i = 0; i < number; i++) {
         if(token[i] == '!' || token[i] == '"' || token[i] == '#' || token[i] == '$' || token[i] == '&' || token[i] ==
-        '\'' ||
-           token[i] == '(' || token[i] == ')' || token[i] == '*' || token[i] == '+'  || token[i] == ',' || token[i]
+        '\'' || token[i] == '(' || token[i] == ')' || token[i] == '*' || token[i] == '+'  || token[i] == ',' || token[i]
            == '-' || token[i] == '.' || token[i] == '/' || token[i] == ':' || token[i] == ';' || token[i] == '?' ||
-           token[i] == '@'
-           || token[i] == '[' || token[i] == '^' || token[i] == '_' || token[i] == '`' || token[i]
+           token[i] == '@' || token[i] == '[' || token[i] == '^' || token[i] == '_' || token[i] == '`' || token[i]
            == '{' || token[i] == '|'  || token[i] == '}' || token[i] == '~') {
-            cout << "the punctuation: " << token[i] << "\n";
+            punctuationCount++;
+            cout << punctuationCount << ")"<< " punctuation: " << token[i] << "\n";
         }
     }
+    if (punctuationCount >=1)  cout << "-------------------\ntotal punctuations: " << punctuationCount << "\n\n" <<endl;
 }
 
 int main() {
     char str[100];
-    cout << "enter a string: ";
+    cout << "\nenter a expression: ";
     cin >> str;
+    number = strlen(str);
+    cout <<"\nexpression characters are: ";
+    for (int i = 0; i < number; i++) {
+        cout << " "<<str[i] <<" ";
+    }
+    cout << "\ntotal expression characters: " << number<<"\n\n";
     checkToken(str);
     return 0;
 }
